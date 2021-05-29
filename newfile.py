2ae4c6069981c7b4b1124e392fcfcea479149c69
Afrikaempire <85017015+Afrikaempire@users.noreply.github.com>
2021-05-29 12:03:44 +0100

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,71 @@
+USSD_CODE = ("GT BANK : *737#")
+print(USSD_CODE)
+display = "Please type in your banks ussd code: 737"
+print(display)
+display_0= "You have entered the code for GTBank of Nigeria"
+print(display_0)
+list = [ "1.Airtime-self",  "2.Airtime-others",  "3.Data", "4.Trsf-GTB", " 5.Trsf-others", "6.Trsf-mfB/ewallet", "7.Cable Tv", "8.Pay Bills", "9.Next"]
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print(list [4])
+print(list [5])
+print(list [6])
+print(list[7])
+print(list [8])
+option_2 = 'Please select an option: 5'
+print (option_2)
+display_7= 'Enter account number you want to transfer money to: 0345475243 '
+print(display_7)
+display_6='Enter Amount : 5200'
+print(display_6)
+display_3= "Enter pin :0518"
+print(display_3)
+display_4= "Do you want to send 5200 to 0345475243?"
+print(display_4)
+option_3= ["1.YES" , "2.NO"]
+print(option_3[0])
+print(option_3[1])
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+display_4 = "TRANSACTION SUCCESSFUL"
+print(display_4)
+option_0=   "ENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "ENTER 2 TO END"
+print(option_05)
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print(list [4])
+print(list [5])
+print(list[6])
+print(list[7])
+print(list[8])
+option_3= "Please select an option: 2"
+print(option_3)
+print(display_4)
+display_6= 'Enter Amount : 200'
+print(display_6)
+display_7= 'Enter  number you want to buy airtime for: 09033341634'
+print(display_7)
+display_4= "Do you want to buy 200 naira worth of airtime for 09033341634?"
+print(display_4)
+option_3= ["1.YES" , "2.NO"]
+print(option_3[0])
+print(option_3[1])
+display_4 = "Enter 1 or 2 : 1"
+print(display_4)
+display_4 = "TRANSACTION SUCCESSFUL"
+print(display_4)
+option_0=   "ENTER 1 TO RETURN TO MENU"
+print(option_0)
+option_05= "ENTER 2 TO END"
+print(option_05) 
+display_4 = "Enter 1 or 2 : 2"
+display_11="An SMS message will be sent to you shortly."
+print(display_11)
